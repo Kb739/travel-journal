576a89d894796188710a4023247c57ce2af54747
kunal <kunalbankoti9@gmail.com>
2022-07-06 10:24:47 +0530

    include components directory

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function card() {
+    return (
+        <section>
+            <img alt='location' />
+            <div>
+                <div>
+                    <img src='../images/Path.png' alt='locationIcon' />
+                    <h3>JAPAN</h3>
+                </div>
+                <h1>Mount Fuji</h1>
+                <h3>Date</h3>
+                <p>kjkdjfjdfdjfdjfdjfdfjdfsfsffs description</p>
+            </div>
+        </section>
+    )
+}
